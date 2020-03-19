2b5a66948611e3121664b1296eda4e6deb40b987
unknown <greysonmrx@gmail.com>
2020-03-19 16:30:20 -0300

    feat: create default layout

---
+++ src/layouts/Default/index.js
@@ -0,0 +1,21 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+import { Wrapper } from "./styles";
+
+import Sidebar from "../../components/Sidebar";
+
+function DefaultLayout({ children }) {
+  return (
+    <Wrapper>
+      <Sidebar />
+      {children}
+    </Wrapper>
+  );
+}
+
+export default DefaultLayout;
+
+DefaultLayout.propTypes = {
+  children: PropTypes.element.isRequired
+};
---
+++ src/layouts/Default/styles.js
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.div`
+  display: flex;
+  height: 100%;
+  background: #ffffff;
+  justify-content: space-between;
+`;
