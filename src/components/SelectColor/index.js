1cd87df04e5eab9057605835a6caf8571b78b586
unknown <greysonmrx@gmail.com>
2020-03-20 01:58:45 -0300

    feat: create selectcolor component

---
+++ src/components/SelectColor/index.js
@@ -0,0 +1,63 @@
+import React, { useEffect, useRef } from "react";
+import PropTypes from "prop-types";
+import { useField } from "@unform/core";
+import { FaCheck } from "react-icons/fa";
+
+import { Container } from "./styles";
+
+function SelectColor({ name, options, style, ...rest }) {
+  const radioRef = useRef([]);
+  const { fieldName, registerField, defaultValue } = useField(name);
+
+  useEffect(() => {
+    registerField({
+      name: fieldName,
+      ref: radioRef.current,
+      path: "value",
+      getValue(refs) {
+        const checked = refs.find(ref => ref.checked);
+
+        return checked ? checked.value : null;
+      },
+      setValue(refs, value) {
+        const item = refs.find(ref => ref.value === value);
+
+        if (item) {
+          item.checked = true;
+        }
+      }
+    });
+  }, [fieldName, registerField]);
+
+  return (
+    <div>
+      {options.map((option, index) => (
+        <Container key={option.color} color={option.color}>
+          <input
+            name={fieldName}
+            type="radio"
+            ref={elRef => (radioRef.current[index] = elRef)}
+            defaultChecked={defaultValue === option.color}
+            value={option.color}
+            {...rest}
+          />
+          <span>
+            <FaCheck />
+          </span>
+        </Container>
+      ))}
+    </div>
+  );
+}
+
+export default SelectColor;
+
+SelectColor.propType = {
+  name: PropTypes.string.isRequired,
+  options: PropTypes.arrayOf(PropTypes.object).isRequired,
+  style: PropTypes.oneOfType([PropTypes.object, PropTypes.array])
+};
+
+SelectColor.defaultProps = {
+  style: {}
+};
---
+++ src/components/SelectColor/styles.js
@@ -0,0 +1,50 @@
+import styled from "styled-components";
+import { darken } from "polished";
+
+export const Container = styled.label`
+  input {
+    display: none;
+
+    &:checked + span {
+      width: 2em;
+      height: 2em;
+      border: 0;
+      opacity: 1;
+
+      svg {
+        opacity: 1;
+        transform: scale(0);
+        color: white;
+        -webkit-text-stroke: 0;
+        opacity: 1;
+        transform: scale(1);
+      }
+    }
+  }
+
+  span {
+    border-radius: 50%;
+    background: ${props => props.color};
+    vertical-align: middle;
+    margin-right: 10px;
+    width: 2em;
+    height: 2em;
+    cursor: pointer;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    transition: all 0.3s ease;
+
+    &:hover {
+      background: ${props => darken(0.15, props.color)};
+    }
+
+    svg {
+      opacity: 0.2;
+      font-size: 16px;
+      color: transparent;
+      transition: opacity 0.3s 0.1s ease;
+      -webkit-text-stroke: 3px rgba(0, 0, 0, 0.5);
+    }
+  }
+`;
