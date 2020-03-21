2154a7945ee974f5504c4750b14f3780d1820018
unknown <greysonmrx@gmail.com>
2020-03-20 22:34:52 -0300

    feat: create modaldelete component

---
+++ src/components/ModalDelete/index.js
@@ -0,0 +1,62 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { FaTimes } from "react-icons/fa";
+import { MdDelete } from "react-icons/md";
+
+import {
+  Container,
+  Modal,
+  Top,
+  Center,
+  Bottom,
+  Content,
+  Icon,
+  Exit,
+  Button
+} from "./styles";
+
+function ModalDelete({ title, message, cancel, confirm, visible }) {
+  return (
+    <Container visible={visible}>
+      <Modal>
+        <Top>
+          <Content>
+            <Icon>
+              <MdDelete />
+            </Icon>
+            <h3>{title}</h3>
+          </Content>
+          <Exit onClick={() => cancel()}>
+            <FaTimes />
+          </Exit>
+        </Top>
+        <Center>
+          <p>{message}</p>
+        </Center>
+        <Bottom>
+          <Button outline onClick={() => cancel()}>
+            Cancelar
+          </Button>
+          <Button
+            onClick={() => {
+              confirm();
+              cancel();
+            }}
+          >
+            Excluir
+          </Button>
+        </Bottom>
+      </Modal>
+    </Container>
+  );
+}
+
+export default ModalDelete;
+
+ModalDelete.propTypes = {
+  title: PropTypes.string.isRequired,
+  message: PropTypes.string.isRequired,
+  visible: PropTypes.bool.isRequired,
+  cancel: PropTypes.func.isRequired,
+  confirm: PropTypes.func.isRequired
+};
---
+++ src/components/ModalDelete/styles.js
@@ -0,0 +1,98 @@
+import styled from "styled-components";
+import { lighten, darken } from "polished";
+
+export const Container = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  display: ${props => (props.visible ? "flex" : "none")};
+  justify-content: center;
+  align-items: center;
+  background: rgba(0, 0, 0, 0.5);
+  z-index: 999999;
+`;
+
+export const Modal = styled.div`
+  background: #ffffff;
+  padding: 20px 30px;
+  border-radius: 8px;
+  width: 500px;
+`;
+
+export const Content = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+export const Top = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  h3 {
+    font-size: 18.5px;
+    margin-top: 5px;
+    margin-left: 15px;
+    color: #202124;
+  }
+`;
+
+export const Center = styled.div`
+  display: flex;
+  align-items: center;
+  padding: 25px 0;
+
+  p {
+    font-size: 16px;
+  }
+`;
+
+export const Bottom = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+`;
+
+export const Icon = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 40px;
+  height: 40px;
+  background: ${lighten(0.5, "#d50000")};
+  border-radius: 50%;
+
+  svg {
+    font-size: 25px;
+    color: #d50000;
+  }
+`;
+
+export const Exit = styled.button`
+  background: none;
+  border: none;
+  outline: none;
+
+  svg {
+    font-size: 20px;
+    color: #999999;
+  }
+`;
+
+export const Button = styled.button`
+  background: ${props => (props.outline ? "#ffffff" : "#d50000")};
+  border: ${props => (props.outline ? "1px solid #bbbbbb" : "none")};
+  padding: 10px 0;
+  width: 90px;
+  margin: 0 5px;
+  border-radius: 5px;
+  font-weight: 500;
+  color: ${props => (props.outline ? "#666666" : "#ffffff")};
+  transition: all 0.3s ease;
+
+  &:hover {
+    background: ${props => darken(0.1, props.outline ? "#ffffff" : "#d50000")};
+  }
+`;
